430f29b09ce28c67ca8fc604de949d14deff9445
Marcus Holmgren <marcus.holmgren1@gmail.com>
2015-02-07 18:47:39 +0100

    Add templating with jinja2.

--- README.md
+++ README.md
@@ -9,6 +9,9 @@ Then using:
 
 [Jinja2][3]
 
+[WTForms][4]
+
+
 ## Licensing
 See [LICENSE](LICENSE)
 
@@ -17,3 +20,4 @@ See [LICENSE](LICENSE)
 [1]: https://github.com/GoogleCloudPlatform/appengine-python-flask-skeleton "A skeleton for creating Python applications using the Flask framework on App Engine"
 [2]: http://flask.pocoo.org "Flask micro framework"
 [3]: http://jinja.pocoo.org "Jinja2 is a full featured template engine for Python"
+[4]: http://wtforms.readthedocs.org/en/latest/# "WTForms Documentation"
--- main.py
+++ main.py
@@ -1,16 +1,55 @@
 """`main` is the top level module for your Flask application."""
 
 # Import the Flask Framework
-from flask import Flask
+from flask import Flask, render_template, session, redirect, url_for, flash
+from flask.ext.wtf import Form
+from wtforms import DecimalField, StringField, SubmitField
+from wtforms.validators import DataRequired
+from google.appengine.ext import ndb
+from config import SECRET_KEY
+
 app = Flask(__name__)
 # Note: We don't need to call run() since our application is embedded within
 # the App Engine WSGI application server.
+app.config['SECRET_KEY'] = SECRET_KEY
+
+class TimeEntry(ndb.Model):
+    code = ndb.StringProperty()
+    time = ndb.FloatProperty()
 
 
-@app.route('/')
-def hello():
-    """Return a friendly HTTP greeting."""
-    return 'Hello World!'
+class DayModule(ndb.Model):
+    day = ndb.StringProperty()
+    date = ndb.DateTimeProperty(auto_now_add=True)
+    details = ndb.LocalStructuredProperty(TimeEntry, repeated=True)
+
+
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    time = DecimalField('Worked hours:', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        app.logger.debug("Form validate name %s" % form.name.data)
+        session['name'] = form.name.data
+        form.name.data = ''
+        return redirect(url_for('index'))
+    #app.logger.debug("Index method called. Name is %s" % session.get("name"))
+    return render_template('index.html', form=form, name=session.get('name'))
+
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    app.logger.debug("Hello method called name: %s" % name)
+    return render_template('index.html', name=name, form=NameForm())
 
 
 @app.errorhandler(404)
